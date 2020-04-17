fa709a9481cdbb655dbf664e61ac466744127940
JimmyMcBride <mcbride967@gmail.com>
2020-04-17 12:13:32 -0500

    it works

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,8 +5,21 @@ Your function must utilize recursion. It cannot contain any loops.
 """
 
 
-def count_th(word):
+def count_th(word, count=0):
+    print(f"The word is: {word}")
+    if word[0:2] == "th":
+        count += 1
+        print(f"The count is: {count}")
+        print(f"The word is: {word[2 : len(word)]}")
+        return count_th(word[2 : len(word)], count)
+    elif len(word[1 : len(word)]) > 1:
+        print(f"Elif: The count is: {count}")
+        print(f"Elif: The word is: {word[1:len(word)]}")
+        print("Elif: recursive call", count_th(word[1 : len(word)], count))
+        count = count_th(word[1 : len(word)], count)
 
-    # TBC
+    print("Final count:", count)
+    return count
 
-    pass
+
+count_th("ththethe")
